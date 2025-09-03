67cbf3cb61592552612681b399d29dbe2a57dac3
Igor Menezes <igormenezes@Mac.localdomain>
2025-09-03 15:35:02 -0300

    Fix: Unifica configuração de path no next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // As linhas abaixo são cruciais para o GitHub Pages funcionar.
-  // Elas garantem que o site encontre seus arquivos (CSS, imagens, etc.) corretamente.
-    basePath: '/futuro-corp-website',
-  assetPrefix: '/futuro-corp-website/',
+  basePath: '/futuro-corp-website',
+  assetPrefix: '/futuro-corp-website',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
