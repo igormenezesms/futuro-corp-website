b89b29ad780347abb8c1c7c7e16f2c043e26624d
Igor Menezes <igormenezes@192.168.0.4>
2025-09-05 11:15:59 -0300

    fix: Adiciona configuração de content ao Tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+export default {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {
+      // Início da configuração do Tailwind
+      content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+      // Fim da configuração do Tailwind
+    },
+    autoprefixer: {},
   },
-}
-
-export default config
+};
